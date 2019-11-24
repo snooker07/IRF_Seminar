ffa274482866b8ce277ad1eea9ebb380eb1a8678
Ben <benjamin.fundelits@gmail.com>
2019-11-24 12:08:56 +0100

    Manages to add new user to the listbox

--- UserMaintenance/UserMaintanance/Form1.cs
+++ UserMaintenance/UserMaintanance/Form1.cs
@@ -7,18 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintanance.Entities;
 
 namespace UserMaintanance
 {
     public partial class Form1 : Form
     {
-
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName; // label1
-            label2.Text = Resource1.FirstName; // label2
+            lblLastName.Text = Resource1.LastName; // label1
+            lblFirstName.Text = Resource1.FirstName; // label2
             button1.Text = Resource1.Add; // button1
+
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtFirstName.Text
+            };
+            users.Add(u);
         }
     }
 }
