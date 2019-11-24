b69d8eb7affd3b5dbb060794c65f3e807b35fddb
Ben <benjamin.fundelits@gmail.com>
2019-11-24 11:22:28 +0100

    Delete .Net core app

---
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserMaintenance.Entities
+{
+    class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName);
+            }
+        }
+
+        // Ugyanaz a FullName property kompaktabb formában is írható
+        /*
+        public string FullName
+            => string.Format(
+                "{0} {1}",
+                LastName,
+                FirstName);
+        */
+    }
+}
